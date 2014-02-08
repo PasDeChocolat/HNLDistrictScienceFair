8bd91236f3afbd4bd163212cfb7a9487dea13f1b
Kyle Oba <koba@mudphone.com>
2014-02-07 18:02:34 -1000

    Add rain.

--- 004_MASHUP.rb
+++ 004_MASHUP.rb
@@ -1,31 +1,23 @@
 load_library "SimpleOpenNI"
 include_package 'SimpleOpenNI'
 
+load_library "pbox2d"
+include_package 'pbox2d'
+include_package 'org.jbox2d.collision.shapes'
+include_package 'org.jbox2d.common'
+include_package 'org.jbox2d.dynamics'
+include_package 'org.jbox2d.dynamics.joints'
+include_package 'org.jbox2d.dynamics.BodyDef'
+include_package 'org.jbox2d.dynamics.contacts'
+include_package 'org.jbox2d.collision.shapes'
+include_package 'org.jbox2d.collision.shapes.Shape'
+
 def setup
   size 1024,768,P3D
   background 0
 
-  @context = SimpleOpenNI.new(self)
-  @zoomF =0.5
-  @rotX = radians(180)  # by default rotate the hole scene 180deg around the x-axis, 
-                        # the data from openni comes upside down
-  @rotY = radians(0)
-
-  if !@context.isInit
-     puts "Can't init SimpleOpenNI, maybe the camera is not connected!"
-     exit
-  end
-
-  @user_positions = []
-
-  # disable mirror
-  @context.setMirror(false)
-
-  # enable depthMap generation 
-  @context.enableDepth()
-
-  # enable skeleton generation for all joints
-  @context.enableUser()
+  setup_kinect
+  setup_box2d
 
   stroke(255,255,255)
   smooth()
@@ -84,8 +76,105 @@ def draw
   userList.each do |u|
     display_ball_for_user(u)
   end
+
+  display_box2d
+
   before_rotation = false
   display_code "hello", before_rotation
+end
+
+def setup_kinect
+  @context = SimpleOpenNI.new(self)
+  @zoomF =0.5
+  @rotX = radians(180)  # by default rotate the hole scene 180deg around the x-axis, 
+                        # the data from openni comes upside down
+  @rotY = radians(0)
+
+  if !@context.isInit
+     puts "Can't init SimpleOpenNI, maybe the camera is not connected!"
+     exit
+  end
+
+  @user_positions = []
+
+  # disable mirror
+  @context.setMirror(false)
+
+  # enable depthMap generation 
+  @context.enableDepth()
+
+  # enable skeleton generation for all joints
+  @context.enableUser()
+end
+
+def setup_box2d
+  # Initialize box2d physics and create the world
+  @box2d = PBox2D.new(self)
+  @box2d.createWorld()
+
+  # Make the box
+  # @boxes = []
+  # 3.times do
+  #   @boxes << Box.new(rand(width),rand(height), @box2d)
+  # end
+  @xoff = 0.0
+  @yoff = 1000.0
+
+  # Create the empty list
+  @particles = []
+end
+
+def display_box2d
+  # x 500
+  # y 200
+  # x_offset = map(mouse_x, 0, width, -1000, 1000)
+  # y_offset = map(mouse_y, 0, height, -1000, 1000)
+  # puts "xoff: #{x_offset}, yoff: #{y_offset}"
+  x_offset = 500
+  y_offset = 200
+  # spread = map(mouse_x, 0, width, 0, 2*width)
+  spread = width - 250
+  puts "spread: #{spread}"
+
+  if (rand(100) < 90)
+    5.times do
+      sz = rand(8) - 4
+      # rain_x = width/2.0
+      rain_x = rand(spread) - x_offset
+      rain_y = y_offset
+      @particles << Particle.new(rain_x, rain_y, sz.to_f, @box2d, height)
+    end
+  end
+
+
+  # We must always step through time!
+  # @box2d.step()
+  @box2d.step(1/60.0, 8, 3)
+  @box2d.setGravity(0, 9.8) # must reverse gravity due to Kinect rotational transform
+
+  # @boxes.each_with_index do |box, index|
+  #   x = noise(@xoff, index)*width
+  #   y = noise(@yoff, index)*height
+  #   box.setLocation(x,y)
+  # end
+  @xoff += 0.01;
+  @yoff += 0.01;
+
+  # Look at all particles
+  (0..(@particles.size-1)).to_a.reverse.each do |i|
+    p = @particles[i]
+    p.display()
+    # Particles that leave the screen, we delete them
+    # (note they have to be deleted from both the box2d world and our list
+    if p.done()
+      @particles.delete_at(i)
+    end
+  end
+
+  # Draw the box
+  # @boxes.each do |box|
+  #   box.display()
+  # end
 end
 
 def display_code code, before_rotation=true
@@ -207,4 +296,97 @@ def drawLimb(userId,jointType1,jointType2)
   line(jointPos1.x,jointPos1.y,jointPos1.z,
        jointPos2.x,jointPos2.y,jointPos2.z)
   # puts "x: #{jointPos1.x}, y: #{jointPos1.y}, z: #{jointPos1.z}"
+end
+
+class Particle
+  include_package 'org.jbox2d.dynamics'
+  include_package 'org.jbox2d.collision.shapes'
+
+  # We need to keep track of a Body and a radius
+  # Body body;
+  # float r;
+  # color col;
+
+  def initialize x, y, r_, box2d_, sketch_height_ 
+    x, y = x.to_f, y.to_f
+    @r = r_.to_f
+    @box2d = box2d_
+    @sketch_height = sketch_height_.to_f
+    # This function puts the particle in the Box2d world
+    @body = makeBody(x, y, @r)  
+    @body.setUserData(self)
+    @col = color(76, 124, 216)
+  end
+
+  # This function removes the particle from the box2d world
+  def killBody
+    @box2d.destroyBody(@body)
+  end
+
+  # Change color when hit
+  def change()
+    @col = color(255, 0, 0)
+  end
+
+  # Is the particle ready for deletion?
+  def done()
+    # Let's find the screen position of the particle
+    pos = @box2d.getBodyPixelCoord(@body)
+    # Is it off the bottom of the screen?
+    if (pos.y > @sketch_height+@r*2)
+      killBody()
+      return true
+    end
+    return false
+  end
+
+  def display()
+    # We look at each body and get its screen position
+    pos = @box2d.getBodyPixelCoord(@body)
+    # Get its angle of rotation
+    a = @body.getAngle()
+
+    push_matrix()
+    translate(pos.x, pos.y)
+    rotate(a)
+    fill(@col)
+    no_stroke
+    ellipse(0, 0, @r*2, @r*2)
+    # Let's add a line so we can see the rotation
+    stroke(0)
+    stroke_weight(1)
+    line(0, 0, @r, 0)
+    pop_matrix()
+  end
+
+  # Here's our function that adds the particle to the Box2D world
+  def makeBody(x, y, r)
+    x, y = x.to_f, y.to_f
+    r = r.to_f
+
+    # Define a body
+    bd = BodyDef.new()
+    # Set its position
+    bd.position = @box2d.coordPixelsToWorld(x, y);
+    bd.type = BodyType::DYNAMIC
+    
+    body = @box2d.createBody(bd)
+
+    # Make the body's shape a circle
+    cs = CircleShape.new()
+    cs.m_radius = @box2d.scalarPixelsToWorld(r)
+
+    fd = FixtureDef.new()
+    fd.shape = cs
+    # Parameters that affect physics
+    fd.density = 1.0;
+    fd.friction = 0.01;
+    fd.restitution = 0.3;
+
+    # Attach fixture to body
+    body.createFixture(fd)
+
+    body.setAngularVelocity(rand(20) - 10)
+    return body
+  end
 end
